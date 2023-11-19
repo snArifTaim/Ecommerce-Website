6bdb982e3fe1dc7c8ffdc7f19781d403040f44ec
Md Arif Islam <ariftaim01@gmail.com>
2023-11-20 00:16:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function onclickMenu() {
+  var x = document.getElementById("sna-navbar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
